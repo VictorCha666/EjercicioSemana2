cd659605026d345f7463903a3ca73ba9e82daeea
vicchabel <iesuda.victorcha@gmail.com>
2025-09-17 18:40:58 +0200

    ÑÑÑÑ

--- Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/App.java
+++ Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/App.java
@@ -1,35 +1,35 @@
 package org.semana01;
 
 import org.semana01.modelos.ColeccionLibros;
+import org.semana01.modelos.Libro;
 
 public class App {
     public static void main(String[] args) {
         ColeccionLibros coleccion = new ColeccionLibros();
+        // Cargar los libros desde un archivo
+        try {
+            java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader("Semana01-ColeccionesYRelaciones/src/main/resources/libros.csv"));
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String[] datos = linea.split(",");
+                coleccion.addLibro(new Libro(datos[0], datos[1], datos[2], Integer.parseInt(datos[3])));
+            }
+            reader.close();
+        } catch (java.io.IOException e) {
+            System.out.println("Error al cargar el archivo de libros");
+            e.printStackTrace();
+        }
 
-        // Libro("9788422616337", "El Señor de los Anillos", "J.R.R. Tolkien", 800);
-        // Libro("9788445077528", "El Hobbit", "J.R.R. Tolkien", 350);
-        // Libro("9788466316781", "Cabo Trafalgar", "Arturo Pérez Reverte", 320);
-        // Libro("9788493975074", "El corazón de la piedra", "José María García López", 560);
-        // Libro("9788493291488", "Salmos de vísperas", "Esteban Hernández Castelló", 95);
-        // Libro("9788420685625", "La música en las catedrales españolas del Siglo de Oro", "Robert Stevenson", 600);
-        // Libro("9788423913077", "Luces de bohemia", "Ramón del Valle-Inclán", 296);
-        // Libro("9788448031121", "Contando atardeceres", "La vecina rubia", 528);
-        // Libro("9781529342079", "The Master: The Brilliant Career of Roger Federer", "Christopher Clarey", 456);
-        // Libro("9788408264385", "La teoría de los archipiélagos", "Alice Kellen", 300);
-        // Libro("9788423362479", "Esperando al diluvio", "Dolores Redondo", 576);
-        // Libro("9788466367349", "El italiano", "Arturo Pérez Reverte", 400);
-        // Libro("9788466359290", "Línea de fuego", "Arturo Pérez Reverte", 688);
-
-//        System.out.println("1) Cantidad de libros con más de 500 páginas: " + coleccion.cantidadLibrosMas500Paginas());
-//        System.out.println("2) Cantidad de libros con menos de 300 páginas: " + coleccion.cantidadLibrosMenos300Paginas());
-//        System.out.println("3) Títulos de libros con más de 500 páginas: " + coleccion.listarLibrosMas500Paginas());
-//        System.out.println("4) Títulos de los 3 libros con más páginas: " + coleccion.listarTresLibrosMasPaginas());
-//        System.out.println("5) Suma de páginas de todos los libros: " + coleccion.sumaTotalPaginas());
-//        System.out.println("6) Libros que superan el promedio de páginas : " + coleccion.listarLibrosMasPaginasPromedio());
-//        System.out.println("7) Autores sin repetir : " + coleccion.listarAutores());
-//        System.out.println("8) Libro con mayor número de páginas: " + coleccion.libroMasPaginas());
-//        System.out.println("9) Títulos de los libros: " + coleccion.listarTitulos());
-//        System.out.println("10) Autores con más de un libro: " + coleccion.listarAutoresConMasDeUnLibro());
+        System.out.println("1) Cantidad de libros con más de 500 páginas: " + coleccion.cantidadLibrosMas500Paginas());
+        System.out.println("2) Cantidad de libros con menos de 300 páginas: " + coleccion.cantidadLibrosMenos300Paginas());
+        System.out.println("3) Títulos de libros con más de 500 páginas: " + coleccion.listarLibrosMas500Paginas());
+        System.out.println("4) Títulos de los 3 libros con más páginas: " + coleccion.listarTresLibrosMasPaginas());
+        System.out.println("5) Suma de páginas de todos los libros: " + coleccion.sumaTotalPaginas());
+        System.out.println("6) Libros que superan el promedio de páginas : " + coleccion.listarLibrosMasPaginasPromedio());
+        System.out.println("7) Autores sin repetir : " + coleccion.listarAutores());
+        System.out.println("8) Libro con mayor número de páginas: " + coleccion.libroMasPaginas());
+        System.out.println("9) Títulos de los libros: " + coleccion.listarTitulos());
+        System.out.println("10) Autores con más de un libro: " + coleccion.listarAutoresConMasDeUnLibro());
 
 
     }
--- Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/modelos/ColeccionLibros.java
+++ Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/modelos/ColeccionLibros.java
@@ -1,7 +1,139 @@
 package org.semana01.modelos;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class ColeccionLibros {
     // ¿Qué tipo de colección es la más adecuada para almacenar los libros?
+    ArrayList<Libro> coleccion;
+
+    public ColeccionLibros() {
+        this.coleccion = new ArrayList<>();
+    }
+
+    public void addLibro(Libro libro) {
+        coleccion.add(libro);
+    }
+
+    public int cantidadLibrosMas500Paginas() {
+        int cont =0;
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas>500)
+                cont++;
+        }
+        return cont;
+    }
+
+    public int cantidadLibrosMenos300Paginas() {
+        int cont =0;
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas<300)
+                cont++;
+        }
+        return cont;
+    }
+
+    public String listarLibrosMas500Paginas() {
+        String listado = "";
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas>500)
+                listado += (coleccion.get(i).titulo+ ", ");
+        }
+        return listado;
+    }
+
+    public String listarTresLibrosMasPaginas() {
+        if (coleccion.size() < 3) return "";
+
+        Libro top1 = coleccion.getFirst();
+        Libro top2 = coleccion.getFirst();
+        Libro top3 = coleccion.getFirst();
+
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas > top1.paginas) {
+                top3 = top2;
+                top2 = top1;
+                top1 = coleccion.get(i);
+            } else if (coleccion.get(i).paginas > top2.paginas && coleccion.get(i) != top1) {
+                top3 = top2;
+                top2 = coleccion.get(i);
+            } else if (coleccion.get(i).paginas > top3.paginas && coleccion.get(i) != top1 && coleccion.get(i) != top2) {
+                top3 = coleccion.get(i);
+            }
+        }
+
+        return top1.titulo + ", " + top2.titulo + ", " + top3.titulo;
+    }
+
+    public int sumaTotalPaginas() {
+        int total=0;
+        for (int i = 0; i < coleccion.size(); i++) {
+            total += coleccion.get(i).paginas;
+        }
+        return total;
+    }
+
+    public String listarLibrosMasPaginasPromedio() {
+        String eu= "";
+        int media = sumaTotalPaginas()/coleccion.size();
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas>media)
+                eu += coleccion.get(i).titulo + ", ";
+        }
+        return eu;
+    }
+
+    public ArrayList<String> evilListarAutores() {
+
+        ArrayList<String> autores = new ArrayList<>();
+        for (int i = 0; i < coleccion.size(); i++) {
+            if (!autores.contains(coleccion.get(i).autor))
+                autores.add(coleccion.get(i).autor);
+        }
+        return autores;
+    }
+
+    public String listarAutores() {
+        String eu="";
+        ArrayList<String> autores = evilListarAutores();
+        for (int i = 0; i < autores.size(); i++) {
+            eu += autores.get(i) + ", ";
+        }
+        return eu;
+        }
+
+    public String libroMasPaginas() {
+        Libro libro = coleccion.getFirst();
+        for (int i = 1; i < coleccion.size(); i++) {
+            if (coleccion.get(i).paginas>libro.paginas)
+                libro = coleccion.get(i);
+        }
+        return libro.titulo;
+    }
+
+    public String listarTitulos() {
+        String dcxz="";
+        for (int i = 0; i < coleccion.size(); i++) {
+            dcxz += coleccion.get(i).titulo +", ";
+        }
+        return dcxz;
+    }
+
+    public String listarAutoresConMasDeUnLibro() {
+        String string="";
+        ArrayList<String> autores = evilListarAutores();
+        for (int i = 0; i < autores.size(); i++) {
+            string += "Autor: " + autores.get(i)+ "; ";
+            for (int j = 0; j < coleccion.size(); j++) {
+                if (Objects.equals(autores.get(i), coleccion.get(j).autor))
+                    string += coleccion.get(j).titulo +", ";
+            }
+            string += "\n";
+        }
+        return string;
+    }
+}
+
 
     // Crea los métodos solicitados en el enunciado del ejercicio
-}
+
